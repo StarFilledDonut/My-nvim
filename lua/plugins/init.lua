be72d55dd8a405bfe6b4e50dec45ec9d1e994a52
StarFilledDonut <kellyisnothere404@gmail.com>
2023-12-31 16:51:11 -0300

    Fixed icons error

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -96,9 +96,6 @@ local default_plugins = {
 
   {
     "nvim-tree/nvim-web-devicons",
-    opts = function()
-      return { override = require "plugins.configs.devicons" }
-    end,
     config = function(_, opts)
       dofile(vim.g.base46_cache .. "devicons")
       require("nvim-web-devicons").setup({
